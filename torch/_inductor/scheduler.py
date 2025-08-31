5487a8c16d41f5890d0a5fe7ba75d29452aa5e39
nullplay <jaeyeon@mit.edu>
2025-08-31 16:22:47 -0400

    fix

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -474,6 +474,9 @@ class BaseSchedulerNode:
     def is_reduction(self) -> bool:
         return False
 
+    def is_native_matmul(self) -> bool:
+        return False 
+
     def is_split_scan(self) -> bool:
         return False
 
@@ -1182,7 +1185,6 @@ class SchedulerNode(BaseSchedulerNode):
         recompute_sizes_body_func: Optional[Callable[_P, _T]] = None,
     ) -> None:
         assert isinstance(self.node, (ir.ComputedBuffer, ir.TemplateBuffer))
-
         self._sizes, body = self.node.simplify_and_reorder(
             extra_indexing_constraints=extra_indexing_constraints,
             recompute_sizes_body_func=recompute_sizes_body_func,
@@ -1339,6 +1341,12 @@ class SchedulerNode(BaseSchedulerNode):
             f"{type(self.node)=}"
         )
         return bool(self.node.get_reduction_type())
+
+    def is_native_matmul(self) -> bool:
+        assert isinstance(self.node, ir.ComputedBuffer), (
+            f"{type(self.node)=}"
+        )
+        return self.node.get_reduction_type() == "dot"
 
     def is_split_scan(self) -> bool:
         assert isinstance(self.node, (ir.ComputedBuffer, ir.TemplateBuffer)), (
@@ -1673,6 +1681,10 @@ class FusedSchedulerNode(BaseSchedulerNode):
     @cache_on_self
     def is_reduction(self) -> bool:
         return any(x.is_reduction() for x in self.snodes)
+
+    @cache_on_self
+    def is_native_matmul(self) -> bool:
+        return any(x.is_native_matmul() for x in self.snodes)
 
     @cache_on_self
     def is_split_scan(self) -> bool:
